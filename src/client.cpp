602cdea4ed72fd11532187de94e2f09c1d5091c8
Anderson Fernandes <fernandesanderson14@gmail.com>
2019-05-14 21:43:59 -0300

    Add initial implementation to shared memory
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- src/client.cpp
+++ src/client.cpp
@@ -1,4 +1,5 @@
 #include <thread>
+#include <vector>
 #include <iostream> 
 #include <sys/ipc.h> 
 #include <sys/shm.h> 
@@ -7,5 +8,14 @@
 using namespace std; 
 
 int main() {
+  key_t key = ftok("shmfile", 65); 
+
+  int shmid = shmget(key,1024,0666|IPC_CREAT); 
+
+  vector<string> *sm = (vector<string>*) shmat(shmid, NULL, 0);
+  (*sm).push_back("TEST");
+
+  shmdt(sm); 
+
   return 0;
 }
--- src/server.cpp
+++ src/server.cpp
@@ -1,4 +1,5 @@
 #include <thread>
+#include <vector>
 #include <iostream> 
 #include <sys/ipc.h> 
 #include <sys/shm.h> 
@@ -6,7 +7,29 @@
 
 using namespace std; 
 
+void clients_sign_in(int id) {
+  cout << "Looking for new clients" << endl; 
+
+  key_t key = ftok("shmfile", 65); 
+
+  int shmid = shmget(key,1024,0666|IPC_CREAT); 
+
+  vector<string> *sm = (vector<string>*) shmat(shmid, NULL, 0);
+
+  string sss = (*sm).front();
+
+  cout << sss;
+
+  shmdt(sm); 
+
+  shmctl(shmid,IPC_RMID,NULL); 
+}
+
 int main() {
   cout << "Starting Server" << endl;
+
+  thread clients_sign_in_thread(clients_sign_in, 1);
+  clients_sign_in_thread.join();
+
   return 0;
 }
