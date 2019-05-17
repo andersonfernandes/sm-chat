c3818fad19401e8b23d67b90b413688b7474a533
Anderson Fernandes <fernandesanderson14@gmail.com>
2019-05-17 17:11:06 -0300

    Add kill server listener thread
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- src/server.cpp
+++ src/server.cpp
@@ -1,3 +1,4 @@
+#include <thread> 
 #include <iostream> 
 #include <sys/ipc.h> 
 #include <sys/shm.h> 
@@ -5,19 +6,29 @@
 
 using namespace std; 
 
+#define CLIENTS_FILE "clients_file"
+
+void kill_server_listener() {
+  string exit_server = "";
+  do {
+    cout << "type exit to stop the server\n" << endl;
+    cin >> exit_server;
+  } while(exit_server.compare("exit"));
+}
+
 int main() {
   cout << "Starting Server" << endl;
-  cout << "Looking for new clients" << endl; 
 
-  key_t key = ftok("shmfile", 65);
+  key_t clients_key = ftok(CLIENTS_FILE, 65);
 
-  int shmid = shmget(key,1024, 0666|IPC_CREAT); 
-  int *sm = (int*) shmat(shmid, NULL, 0);
+  int clients_shmid = shmget(clients_key, 1024, 0666|IPC_CREAT); 
+  int *clients = (int*) shmat(clients_shmid, NULL, 0);
 
-  cout << sm[0] << endl;
+  thread kill_server_listener_thread(kill_server_listener);
+  kill_server_listener_thread.join();
 
-  shmdt(sm); 
-  shmctl(shmid, IPC_RMID, NULL);
+  shmdt(clients); 
+  shmctl(clients_shmid, IPC_RMID, NULL);
 
   return 0;
 }
