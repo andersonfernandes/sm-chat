73844afcc9bda3660ab783ef55f051c5b42cafe1
Anderson Fernandes <fernandesanderson14@gmail.com>
2019-06-08 16:22:27 -0300

    Add shmq tests
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- Makefile
+++ Makefile
@@ -9,4 +9,4 @@ clean:
 
 test:
 	g++ -std=c++11 tests/shm_queue_test.cpp src/shm_queue.cpp -o tests/bin/shm_queue_test
-	./tests/bin/shm_queue_test --success --reporter compact
+	./tests/bin/shm_queue_test --success
--- tests/shm_queue_test.cpp
+++ tests/shm_queue_test.cpp
@@ -1,37 +1,74 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.h"
 
+#include <sys/ipc.h>
+#include <sys/shm.h>
 #include "../include/shm_queue.h"
 
-ShmQueue* shmq;
+void clear_setup(ShmQueue* shmq);
+Message* provide_message();
 
 SCENARIO("SHM Queue") {
+  ShmQueue* shmq = create_queue();
+  Message* message = provide_message();
+
   WHEN("#create_queue") {
-    shmq = create_queue();
-
-    THEN("shmq pointer cant be NULL") {
-      REQUIRE(shmq != NULL);
-    }
-
-    THEN("capacity should be MAX_MESSAGES") {
-      REQUIRE(shmq->capacity == MAX_MESSAGES);
-    }
-
-    THEN("size and front should be 0") {
-      REQUIRE(shmq->size == 0);
-      REQUIRE(shmq->front == 0);
-    }
-
-    THEN("rear should be MAX_MESSAGES-1") {
-      REQUIRE(shmq->rear == (MAX_MESSAGES-1));
-    }
-
-    THEN("locked should be false") {
-      REQUIRE(shmq->locked == false);
-    }
-
-    THEN("messages_list_shmid should not be -1") {
-      REQUIRE(shmq->messages_list_shmid != -1);
+    THEN("shmq pointer cant be NULL and the default attributes should be set") {
+      CHECK(shmq != NULL);
+      CHECK(shmq->capacity == MAX_MESSAGES);
+      CHECK(shmq->size == 0);
+      CHECK(shmq->front == 0);
+      CHECK(shmq->messages_list_shmid != -1);
     }
   }
+
+  WHEN("#empty") {
+    CHECK(empty(shmq)); 
+  }
+
+  WHEN("#enqueue") {
+    enqueue(shmq, message);
+
+    CHECK(shmq->size == 1);
+    CHECK(shmq->front == shmq->rear);
+  }
+
+  WHEN("#dequeue") {
+    Message* dequeued_message = new Message();
+    enqueue(shmq, message);
+
+    CHECK(dequeue(shmq, dequeued_message));
+    CHECK(shmq->size == 0);
+    CHECK(shmq->front == 1);
+    CHECK(shmq->rear == 0);
+    CHECK(strcmp(dequeued_message->source_name, message->source_name) == 0);
+    CHECK(strcmp(dequeued_message->text, message->text) == 0);
+    CHECK(dequeued_message->mode == message->mode);
+    CHECK(dequeued_message->sent_at == message->sent_at);
+  }
+
+  WHEN("#full") {
+    for (int i = 0; i < MAX_MESSAGES; i++) {
+      enqueue(shmq, provide_message());
+    }
+
+    CHECK(full(shmq)); 
+  }
+
+  clear_setup(shmq);
+}
+
+Message* provide_message() {
+  Message* message = new Message();
+  strcpy(message->source_name, "John Doe");
+  strcpy(message->text, "Hello");
+  message->mode = BROADCAST;
+  message->sent_at = time(0);
+
+  return message;
+}
+
+void clear_setup(ShmQueue* shmq) {
+  shmctl(shmq->messages_list_shmid, IPC_RMID, NULL);
+  free(shmq);
 }
